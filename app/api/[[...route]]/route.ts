c499079434546a56e07c639e1c98a8926613077b
suyashpotdar <suyashpotdar03@gmail.com>
2025-07-11 12:28:12 +0530

    accounts get api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,29 +1,27 @@
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app
-    .get(
-        '/hello', 
-        clerkMiddleware(),
-        (c) => {
-            const auth = getAuth(c);
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
 
-            if (!auth?.userId) {
-                return c.json({ error: "Unauthorized" });
-            }
+    return c.json({
+        error: "Internal Server Error"
+    }, 500);
+});
 
-            return c.json({
-                message: 'Hello, Next.js',
-                userId: auth.userId
-            });
-    });
+const routes = app
+    .route('/accounts', accounts);
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes;
