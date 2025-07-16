6a2a98ec43767c58e871663959195e006997942b
suyashpotdar <suyashpotdar03@gmail.com>
2025-07-16 17:27:18 +0530

    accounts post api backend integration complete

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
-  return <div>Dashboard</div>;
+  const { onOpen } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
+    </div>
+  )
 }
